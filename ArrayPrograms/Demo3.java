99547be277701f90c44d5310cd27ddef2286d7c0
Aashiqui <ashikmail2747@gmail.com>
2024-08-31 23:21:04 +0530

    distinct color added

--- ArrayPrograms/Demo3.java
+++ ArrayPrograms/Demo3.java
@@ -1,21 +1,22 @@
 //? Joining two array
 public class Demo3 {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3};
-        int[] arr2 = {4, 5, 6};
-        
+        int[] arr1 = { 1, 2, 3 };
+        int[] arr2 = { 4, 5, 6 };
+
         int[] result = new int[arr1.length + arr2.length];
-        
+
         for (int i = 0; i < arr1.length; i++) {
             result[i] = arr1[i];
         }
-        
+
         for (int i = 0; i < arr2.length; i++) {
             result[arr1.length + i] = arr2[i];
         }
-        
+
         System.out.print("Joined Array: ");
         for (int i = 0; i < result.length; i++) {
             System.out.print(result[i] + " ");
+        }
     }
 }
---
+++ StringProgram/StringLearningDemo27.java
@@ -0,0 +1,20 @@
+public class StringLearningDemo27 {
+    public static void main(String[] args) {
+        String s="aabbccd";
+        String output=removeDuplicates(s);
+        System.out.println(output);
+    }
+    public static String removeDuplicates(String s)
+    {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            char currChar=s.charAt(i);
+            if(sb.indexOf(String.valueOf(currChar))==-1)
+            {
+                sb.append(currChar);
+            }
+        }
+        return sb.toString();
+    }
+}
